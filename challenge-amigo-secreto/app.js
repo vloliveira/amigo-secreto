2fba94764b2a30d9ae545973fad3cff37e785fa0
vloliveira <vitorloliveira96@gmail.com>
2025-02-21 18:08:31 -0300

    feat: funcao adicionarAmigo implementada

--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -1,1 +1,9 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+  let amigo = document.getElementById("amigo").value;
+  amigos.push(amigo);
+  console.log(amigo, amigos);
+}
